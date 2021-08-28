9c61c72b8cb28fecfe9183805bf33802e70b13f6
Dharmendra Sahu <dknitk@rediffmail.com>
2021-08-28 13:20:34 -0400

    Spring boot JWt

---
+++ kwjwtauth/src/main/java/com/kw/kwjwtauth/KwjwtauthApplication.java
@@ -0,0 +1,13 @@
+package com.kw.kwjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwjwtauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwjwtauthApplication.class, args);
+	}
+
+}
---
+++ kwjwtauth/src/test/java/com/kw/kwjwtauth/KwjwtauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kw.kwjwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwjwtauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
